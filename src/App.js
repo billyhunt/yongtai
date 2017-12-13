464ab14d6933743d764fffd545754bb333e377f1
Billy Hunt <bhunt@silvertree.com>
2017-12-13 15:28:48 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './yongtai.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+          <img src={logo}/>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
